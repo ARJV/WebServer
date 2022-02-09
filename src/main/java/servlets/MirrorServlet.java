8a48671de195a66d6223f959f49affd2b530edb0
yashn <yahnovag@gmailc.com>
2022-02-10 00:05:10 +0300

    Закончена первая практика

---
+++ src/main/java/servlets/MirrorServlet.java
@@ -0,0 +1,16 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MirrorServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println(request.getParameter("key"));
+    }
+}
